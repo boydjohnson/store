81c86712129a6ed55616cc2294cf6478d5fbb487
Boyd Johnson <johnson.boyd@gmail.com>
2019-12-29 12:23:23 -0600

    Add iter methods to store, make T a phantom type
    Before T was speficied on each function which could lead to wonky
    behavior. You could `insert` a Stop (for example) and then `get` a Trip.

--- src/error.rs
+++ src/error.rs
@@ -3,4 +3,5 @@ use leveldb::database::error::Error;
 pub enum StoreError {
     DatabaseError(Error),
     SerDeError(bincode::Error),
+    FileSystemError(std::io::Error),
 }
--- src/store.rs
+++ src/store.rs
@@ -1,30 +1,37 @@
 use crate::error::StoreError;
 use bincode::{deserialize, serialize};
 use db_key::Key;
-use leveldb::database::Database;
+use leveldb::database::{iterator::Iterable, Database};
 use leveldb::kv::KV;
 use leveldb::options::{Options, ReadOptions, WriteOptions};
 use serde::{Deserialize, Serialize};
-use std::hash::Hash;
 use std::path::Path;
 
-pub struct Store<K: Key + Eq + Hash> {
+pub struct Store<K: Key, T> {
     db: Database<K>,
+
+    phantom: std::marker::PhantomData<T>,
 }
 
-impl<K: Key + Eq + Hash> Store<K> {
-    pub fn new(path: &Path) -> Result<Store<K>, StoreError> {
+impl<K: Key, T> Store<K, T>
+where
+    T: Serialize + for<'de> Deserialize<'de>,
+{
+    pub fn new(path: &Path) -> Result<Store<K, T>, StoreError> {
         let mut options = Options::new();
         options.create_if_missing = true;
         Ok(Store {
             db: Database::open(path, options).map_err(|err| StoreError::DatabaseError(err))?,
+            phantom: std::marker::PhantomData::default(),
         })
     }
 
-    pub fn insert<T>(&mut self, key: K, value: T) -> Result<(), StoreError>
-    where
-        T: Serialize,
-    {
+    pub fn new_store(prefix: &str) -> Result<Store<K, T>, StoreError> {
+        let tmp = tempdir::TempDir::new(prefix).map_err(|err| StoreError::FileSystemError(err))?;
+        Store::new(tmp.path())
+    }
+
+    pub fn insert(&mut self, key: K, value: T) -> Result<(), StoreError> {
         self.db
             .put(
                 WriteOptions::new(),
@@ -34,24 +41,48 @@ impl<K: Key + Eq + Hash> Store<K> {
             .map_err(|err| StoreError::DatabaseError(err))
     }
 
-    pub fn get<T>(&self, key: &K) -> Result<Option<T>, StoreError>
-    where
-        T: for<'de> Deserialize<'de>,
-    {
-        let bytes;
-        match self.db.get(ReadOptions::new(), key) {
-            Ok(option) => {
-                if let Some(value_bytes) = option {
-                    bytes = value_bytes;
-                    match deserialize(&bytes) {
-                        Err(err) => Err(StoreError::SerDeError(err)),
-                        Ok(item) => Ok(Some(item)),
-                    }
-                } else {
-                    Ok(None)
-                }
-            }
+    pub fn get(&self, key: &K) -> Result<Option<T>, StoreError> {
+        let result = self.db.get(ReadOptions::new(), key);
+
+        match result {
+            Ok(option) => Store::<K, T>::deserialize(option),
             Err(err) => Err(StoreError::DatabaseError(err)),
         }
+    }
+
+    pub fn iter_keys<'a>(&'a self) -> impl Iterator<Item = K> + 'a {
+        self.db.keys_iter(ReadOptions::new())
+    }
+
+    pub fn iter<'a>(&'a self) -> impl Iterator<Item = (K, T)> + 'a {
+        self.db.iter(ReadOptions::new()).filter_map(|(key, value)| {
+            match Store::<K, T>::deserialize_bytes(&value) {
+                Ok(val) => Some((key, val)),
+                Err(_) => None,
+            }
+        })
+    }
+
+    pub fn iter_values<'a>(&'a self) -> impl Iterator<Item = T> + 'a {
+        self.db
+            .value_iter(ReadOptions::new())
+            .filter_map(|item| Store::<K, T>::deserialize_bytes(&item).ok())
+    }
+
+    fn deserialize(option: Option<Vec<u8>>) -> Result<Option<T>, StoreError> {
+        let bytes;
+        if let Some(value_bytes) = option {
+            bytes = value_bytes;
+            match deserialize(&bytes) {
+                Err(err) => Err(StoreError::SerDeError(err)),
+                Ok(item) => Ok(Some(item)),
+            }
+        } else {
+            Ok(None)
+        }
+    }
+
+    fn deserialize_bytes(bytes: &[u8]) -> Result<T, StoreError> {
+        deserialize(bytes).map_err(|err| StoreError::SerDeError(err))
     }
 }
